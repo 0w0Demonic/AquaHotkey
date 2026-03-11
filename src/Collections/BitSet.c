1228165a31606ed4c9c3cde626fcaa3ac588a863
0w0Demonic <paulroemer14.05@gmail.com>
2026-03-11 15:34:04 +0100

    fix(BitSet): fallback if POPCNT unsupported

--- src/Collections/BitSet.c
+++ src/Collections/BitSet.c
@@ -4,22 +4,52 @@
 
 #include <intrin.h>
 #include <stdint.h>
+#include <windows.h>
 
-/**
- * @brief Counts the number of set bits in a buffer.
- * @param data pointer to memory buffer
- * @param len number of bytes in the buffer
- * @returns numbers of bits set to 1
- */
-__declspec(dllexport)
-size_t popcount(
-        const uint8_t* data,
-        size_t len
-) {
-    if (!data || !len) {
-        return 0;
+#define CPUID_BASIC_INFO 0
+#define CPUID_FEATURES   1
+
+#define EAX(info) ((info)[0])
+#define EBX(info) ((info)[1])
+#define ECX(info) ((info)[2])
+#define EDX(info) ((info)[3])
+
+#define POPCNT (1u << 23)
+
+// determines whether the CPU supports POPCNT
+static BOOL gSupportsPopcnt = FALSE;
+
+// check for POPCNT support via CPUID
+static void checkSupportForPopcnt(void)
+{
+    int info[4];
+
+    __cpuid(info, CPUID_BASIC_INFO);
+    if (CPUID_FEATURES <= EAX(info)) {
+        __cpuid(info, CPUID_FEATURES);
+        gSupportsPopcnt = ECX(info) & POPCNT;
     }
+}
 
+// DLL entry point
+BOOL WINAPI
+DllMain(HINSTANCE hinstDLL,
+        DWORD fdwReason,
+        LPVOID lpvReserved)
+{
+    UNREFERENCED_PARAMETER(hinstDLL);
+    UNREFERENCED_PARAMETER(lpvReserved);
+    if (fdwReason == DLL_PROCESS_ATTACH) {
+        checkSupportForPopcnt();
+    }
+    return TRUE;
+}
+
+// "standard" implementation through POPCNT'ing things in blocks of 64 bit
+static size_t popcount_std(
+        const uint8_t* data,
+        size_t len)
+{
     size_t count = 0;
     const uint64_t* p64 = (const uint64_t*) data;
     size_t n64 = (len / 8);
@@ -34,4 +64,40 @@ size_t popcount(
     }
 
     return count;
+}
+
+// fallback implementation, if CPU happens to be absolutely ancient.
+static size_t popcount_fallback(
+        const uint8_t* data,
+        size_t len)
+{
+    // table of all bytes -> population count
+    #define B2(n)    n,     n+1,     n+1,     n+2
+    #define B4(n) B2(n), B2(n+1), B2(n+1), B2(n+2)
+    #define B6(n) B4(n), B4(n+1), B4(n+1), B4(n+2)
+
+    static const uint8_t table[256] = { B6(0), B6(1), B6(1), B6(2) };
+
+    size_t count = 0;
+    for (size_t i = 0; i < len; i++) {
+        count += table[data[i]];
+    }
+    return count;
+}
+
+// determines the population count (1-bits) of the given segment in memory.
+__declspec(dllexport)
+size_t popcount(
+        const uint8_t* data,
+        size_t len)
+{
+    if (!data || !len) {
+        return 0;
+    }
+
+    if (gSupportsPopcnt) {
+        return popcount_std(data, len);
+    } else {
+        return popcount_fallback(data, len);
+    }
 }
--- tests/sandbox.ahk
+++ tests/sandbox.ahk
@@ -1,3 +1,7 @@
 ; << for quick one-off tests >>
 #Requires AutoHotkey v2.0
 #Include <AquaHotkeyX>
+
+#Include <AquaHotkey\src\Collections\BitSet>
+
+BitSet(0, 1, 2, 3, 4, 5).Size.MsgBox()
